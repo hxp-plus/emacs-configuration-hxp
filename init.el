44d2d3ec6cdcb08567823517e97bca4a168db1bf
Xiping Hu <XipingHu@hust.edu.cn>
2020-01-05 16:01:53 +0800

    Added Snippets

--- init.el
+++ init.el
@@ -14,9 +14,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "ba913d12adb68e9dadf1f43e6afa8e46c4822bb96a289d5bf1204344064f041e" default)))
+    ("d75b09e9a0760034d871fde1ef96b55826d63dafef577d14a01b690764c851e9" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "ba913d12adb68e9dadf1f43e6afa8e46c4822bb96a289d5bf1204344064f041e" default)))
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/dict/dict.txt")
+ '(latex-block-names nil)
  '(package-archives
    (quote
     (("melpa-stable" . "https://stable.melpa.org/packages/")
@@ -34,7 +35,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(preview-face ((t nil)))
+ '(preview-reference-face ((t nil))))
 
 (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex)
 (setq TeX-auto-save t)
@@ -68,6 +70,9 @@
 ;; Show line numbers
 ;; (global-linum-mode t)
 
+;; Set font
+(set-frame-font "hack" nil t)
+
 ;; Enable yasnippet
 (yas-global-mode t)
 
@@ -89,6 +94,8 @@
      (browse-url url)))
  (setq flymd-browser-open-function 'my-flymd-browser-function)
 
-;; (load-theme 'dracula t)
+(load-theme 'dracula t)
+
+
 
 (setq package-check-signature nil)
