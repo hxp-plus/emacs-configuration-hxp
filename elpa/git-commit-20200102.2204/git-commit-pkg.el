00d7d6339a8cd2944bf8da7f067440bd428c67bf
Xiping Hu <XipingHu@hust.edu.cn>
2020-01-10 15:40:19 +0800

    Added magit

---
+++ elpa/git-commit-20200102.2204/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200102.2204" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "f6307724251b1632efb7607d187dd8ccc602ae55" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
