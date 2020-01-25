a26a34f0a56752e9365c9866aa0c4e01e18d08a6
Xiping Hu <XipingHu@hust.edu.cn>
2020-01-25 20:10:04 +0800

    Added octave support

--- init.el
+++ init.el
@@ -162,6 +162,17 @@
 (add-hook 'asm-mode-hook 'nasm-mode)
 
 
+;; Octave-mode
+(autoload 'octave-mode "octave-mod" nil t)
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(add-hook 'octave-mode-hook
+          (lambda ()
+            (abbrev-mode 1)
+            (auto-fill-mode 1)
+            (if (eq window-system 'x)
+                (font-lock-mode 1))))
+
 ;; Magit
 (global-set-key (kbd "C-x g") 'magit-status)
 
