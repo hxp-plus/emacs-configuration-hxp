fb6af08458d39c6c1f5ba53bef54e0326ce85701
Xiping Hu <XipingHu@hust.edu.cn>
2020-09-18 20:53:28 +0800

    c

---
+++ elpa/fcitx-20190806.1923/fcitx-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/hxp/.emacs.d/elpa/fcitx-20190806.1923/fcitx.el  -*- no-byte-compile: t -*-
+(define-package "fcitx" "20190806.1923" "Make fcitx better in Emacs" 'nil :commit "12dc2638ddd15c8f6cfaecb20e1f428ab2bb5624" :keywords '("extensions") :authors '(("Junpeng Qiu" . "qjpchmail@gmail.com")) :maintainer '("Junpeng Qiu" . "qjpchmail@gmail.com") :url "https://github.com/cute-jumper/fcitx.el")
--- init.el
+++ init.el
@@ -13,17 +13,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("28caf31770f88ffaac6363acfda5627019cac57ea252ceb2d41d98df6d87e240" "d75b09e9a0760034d871fde1ef96b55826d63dafef577d14a01b690764c851e9" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "ba913d12adb68e9dadf1f43e6afa8e46c4822bb96a289d5bf1204344064f041e" default)))
+   '("28caf31770f88ffaac6363acfda5627019cac57ea252ceb2d41d98df6d87e240" "d75b09e9a0760034d871fde1ef96b55826d63dafef577d14a01b690764c851e9" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "ba913d12adb68e9dadf1f43e6afa8e46c4822bb96a289d5bf1204344064f041e" default))
  '(inhibit-startup-screen t)
  '(ispell-personal-dictionary "~/.emacs.d/dict/dict.txt")
  '(latex-block-names nil)
  '(package-selected-packages
-   (quote
-    (cmake-ide ac-rtags helm-projectile flycheck-rtags company-rtags helm-rtags rtags irony-eldoc flycheck-irony company-irony-c-headers company-irony company-c-headers exec-path-from-shell go-autocomplete go-mode evil smartparens dockerfile-mode nasm-mode markdown-preview-eww neotree ac-python ac-html-csswatcher ac-html-bootstrap ac-html-angular ac-html ac-ispell ac-octave matlab-mode jedi ein auctex xref-js2 js2-refactor js2-mode cnfonts web-mode anaconda-mode magit py-autopep8 flycheck elpygen org-link-minor-mode impatient-mode flymd gnu-elpa-keyring-update dracula-theme list-packages-ext elpy python-django django-mode markdown-mode yasnippet-snippets pdf-tools ac-math company-math auto-complete-auctex constant-theme cdlatex)))
+   '(fcitx pyim cmake-ide ac-rtags helm-projectile flycheck-rtags company-rtags helm-rtags rtags irony-eldoc flycheck-irony company-irony-c-headers company-irony company-c-headers exec-path-from-shell go-autocomplete go-mode evil smartparens dockerfile-mode nasm-mode markdown-preview-eww neotree ac-python ac-html-csswatcher ac-html-bootstrap ac-html-angular ac-html ac-ispell ac-octave matlab-mode jedi ein auctex xref-js2 js2-refactor js2-mode cnfonts web-mode anaconda-mode magit py-autopep8 flycheck elpygen org-link-minor-mode impatient-mode flymd gnu-elpa-keyring-update dracula-theme list-packages-ext elpy python-django django-mode markdown-mode yasnippet-snippets pdf-tools ac-math company-math auto-complete-auctex constant-theme cdlatex))
  '(preview-gs-options
-   (quote
-    ("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4" "-dNOSAFER")))
+   '("-q" "-dNOPAUSE" "-DNOPLATFONTS" "-dPrinted" "-dTextAlphaBits=4" "-dGraphicsAlphaBits=4" "-dNOSAFER"))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
